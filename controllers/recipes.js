4ffa1ab1e019cb3e12d4b82b422ab70324fceed4
Mathew <muckele7@gmail.com>
2024-01-22 15:56:09 -0500

    revamps show.ejs page

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -37,7 +37,7 @@ function create(req, res) {
 }
 
 function show(req, res) {
-    Recipe.findById(req.params.tacoId)
+    Recipe.findById(req.params.recipeId)
     .populate("owner")
     .then(recipe => {
         res.render('recipes/show', {
