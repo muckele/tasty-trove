ab879c242ee55818a1f12f6bf5d045fa005de43f
Mathew <muckele7@gmail.com>
2024-01-22 10:48:47 -0500

    creates javascript directory and creates new.js file

---
+++ public/javascript/new.js
@@ -0,0 +1,13 @@
+document.getElementById('preparation-list').addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    event.preventDefault()
+    addNewListItem()
+  }
+})
+
+function addNewListItem() {
+  let ol = document.getElementById('preparation-list')
+  let newLi = document.createElement('li')
+  newLi.innerHTML = '<input type="text" name="preparationStep' + (ol.childElementCount + 1) + '" placeholder="Step ' + (ol.childElementCount + 1) + '">'
+  ol.appendChild(newLi)
+}
