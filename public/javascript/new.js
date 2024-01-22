a4b02c4f1aa63c36570c0934e44d50528fdfccae
Mathew <muckele7@gmail.com>
2024-01-22 15:22:52 -0500

    updates show.ejs view

--- public/javascript/new.js
+++ public/javascript/new.js
@@ -1,13 +1,11 @@
-document.getElementById('preparation-list').addEventListener('keydown', function(event) {
-  if (event.key === 'Enter') {
-    event.preventDefault()
-    addNewListItem()
-  }
-})
+document.getElementById('add-step-button').addEventListener('click', function() {
+  console.log('Button clicked!');
+  addNewListItem();
+});
 
 function addNewListItem() {
-  let ol = document.getElementById('preparation-list')
-  let newLi = document.createElement('li')
-  newLi.innerHTML = '<input type="text" name="preparation[]' + (ol.childElementCount + 1) + '" placeholder="Step ' + (ol.childElementCount + 1) + '">'
-  ol.appendChild(newLi)
+  let ol = document.getElementById('preparation-list');
+  let newLi = document.createElement('li');
+  newLi.innerHTML = '<input type="text" name="preparation[]' + (ol.childElementCount + 1) + '" placeholder="Step ' + (ol.childElementCount + 1) + '">';
+  ol.appendChild(newLi);
 }
